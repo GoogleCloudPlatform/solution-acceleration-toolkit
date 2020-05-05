1e53ddec556b15ae62c90914a90defb690aa0445
Martin Petkov <mpetkov@google.com>
2020-05-05 10:49:31 -0400

    Importer: Add google_organization_iam_member

---
+++ internal/tfimport/importer/google_organization_iam_member.go
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package importer
+
+import (
+	"fmt"
+
+	"github.com/GoogleCloudPlatform/healthcare-data-protection-suite/internal/terraform"
+)
+
+// OrgIamMember defines a struct with the necessary information for a GKE cluster to be imported.
+type OrgIamMember struct{}
+
+// ImportID returns the GCP project and bucket name for use in importing.
+func (b *OrgIamMember) ImportID(rc terraform.ResourceChange, pcv ProviderConfigMap) (string, error) {
+	orgID, err := fromConfigValues("org_id", rc.Change.After, pcv)
+	if err != nil {
+		return "", err
+	}
+
+	role, err := fromConfigValues("role", rc.Change.After, pcv)
+	if err != nil {
+		return "", err
+	}
+
+	member, err := fromConfigValues("member", rc.Change.After, pcv)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%v %v %v", orgID, role, member), nil
+}
