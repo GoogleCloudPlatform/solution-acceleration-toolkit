e4aa6bcc877d2100727d7c8e4239d984651e041f
Martin Petkov <MartinPetkov@users.noreply.github.com>
2020-05-05 17:03:31 -0400

    Importer: Add google_organization_iam_audit_config (#42)
    * Importer: Add google_organization_iam_audit_config
    
    * Undo change to skip errors
    
    On second thought, we should keep errors. All other situations should be dealt with in the code.

--- internal/tfimport/tfimport.go
+++ internal/tfimport/tfimport.go
@@ -35,10 +35,11 @@ var (
 // Defines all supported resource importers
 // TODO: Add more resources
 var importers = map[string]resourceImporter{
-	"google_storage_bucket":          &importer.StorageBucket{},
-	"google_container_cluster":       &importer.GKECluster{},
-	"google_organization_policy":     &importer.OrgPolicy{},
-	"google_organization_iam_member": &importer.OrgIAMMember{},
+	"google_storage_bucket":                &importer.StorageBucket{},
+	"google_container_cluster":             &importer.GKECluster{},
+	"google_organization_policy":           &importer.OrgPolicy{},
+	"google_organization_iam_member":       &importer.OrgIAMMember{},
+	"google_organization_iam_audit_config": &importer.OrgIAMAuditConfig{},
 }
 
 // Resource represents a resource and an importer that can import it.
