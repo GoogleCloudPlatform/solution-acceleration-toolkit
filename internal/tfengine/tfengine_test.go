e5b2fd89177ea82dfacd8b3cba983f7d3be851c6
umairidris <umairidris@google.com>
2020-05-07 19:00:34 -0400

    clearer

--- internal/tfengine/tfengine_test.go
+++ internal/tfengine/tfengine_test.go
@@ -11,15 +11,16 @@ const basePath = "../../examples/tfengine"
 
 // TestExamples is a basic test for the engine to ensure it runs without error on the examples.
 func TestExamples(t *testing.T) {
-	for _, c := range []string{"simple", "full"} {
-		t.Run(c, func(t *testing.T) {
+	exs = []string{"simple", "full"}
+	for _, ex := range exs {
+		t.Run(ex, func(t *testing.T) {
 			tmp, err := ioutil.TempDir("", "")
 			if err != nil {
 				t.Fatalf("ioutil.TempDir: %v", err)
 			}
 			defer os.RemoveAll(tmp)
 
-			p := filepath.Join(basePath, c+".yaml")
+			p := filepath.Join(basePath, ex+".yaml")
 			if err := Run(p, tmp); err != nil {
 				t.Fatalf("tfengine.Run(%q, %q) = %v", p, tmp, err)
 			}
