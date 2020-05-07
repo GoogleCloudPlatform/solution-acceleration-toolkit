68491b7e744d1f2e3bdecf4eec375cdcccdfcde3
umairidris <umairidris@google.com>
2020-05-07 19:05:14 -0400

    stat

--- internal/tfengine/tfengine_test.go
+++ internal/tfengine/tfengine_test.go
@@ -11,7 +11,7 @@ const basePath = "../../examples/tfengine"
 
 // TestExamples is a basic test for the engine to ensure it runs without error on the examples.
 func TestExamples(t *testing.T) {
-	exs = []string{"simple", "full"}
+	exs := []string{"simple", "full"}
 	for _, ex := range exs {
 		t.Run(ex, func(t *testing.T) {
 			tmp, err := ioutil.TempDir("", "")
@@ -23,6 +23,12 @@ func TestExamples(t *testing.T) {
 			p := filepath.Join(basePath, ex+".yaml")
 			if err := Run(p, tmp); err != nil {
 				t.Fatalf("tfengine.Run(%q, %q) = %v", p, tmp, err)
+			}
+
+			// Check for the existence of the bootstrap folder.
+			p = filepath.Join(tmp, "bootstrap")
+			if _, err := os.Stat(p); err != nil {
+				t.Fatalf("os.Stat(%q) = %v", p, err)
 			}
 		})
 	}
