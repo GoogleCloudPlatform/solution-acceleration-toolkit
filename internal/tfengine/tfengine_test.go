4de712da631e8c69d71dbafe91e40ddc204aa47e
umairidris <umairidris@google.com>
2020-05-07 18:58:07 -0400

    move to internal

--- internal/tfengine/tfengine_test.go
+++ internal/tfengine/tfengine_test.go
@@ -1,17 +1,16 @@
-package main
+package tfengine
 
 import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"testing"
-
-	"github.com/GoogleCloudPlatform/healthcare-data-protection-suite/internal/tfengine"
 )
 
 const basePath = "../../examples/tfengine"
 
-func TestRun(t *testing.T) {
+// TestExamples is a basic test for the engine to ensure it runs without error on the examples.
+func TestExamples(t *testing.T) {
 	for _, c := range []string{"simple", "full"} {
 		t.Run(c, func(t *testing.T) {
 			tmp, err := ioutil.TempDir("", "")
@@ -21,7 +20,7 @@ func TestRun(t *testing.T) {
 			defer os.RemoveAll(tmp)
 
 			p := filepath.Join(basePath, c+".yaml")
-			if err := tfengine.Run(p, tmp); err != nil {
+			if err := Run(p, tmp); err != nil {
 				t.Fatalf("tfengine.Run(%q, %q) = %v", p, tmp, err)
 			}
 		})
