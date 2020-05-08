11ce8ca4ef99a6b397209541fd4ac47e17937ce8
Martin Petkov <mpetkov@google.com>
2020-05-08 10:07:56 -0400

    Importer: Add google_compute_network

--- internal/tfimport/tfimport.go
+++ internal/tfimport/tfimport.go
@@ -47,6 +47,7 @@ var importers = map[string]resourceImporter{
 	"google_service_account":               &importer.ServiceAccount{},
 	"google_bigquery_table":                &importer.BigQueryTable{},
 	"google_bigquery_dataset":              &importer.BigQueryDataset{},
+	"google_compute_network":               &importer.ComputeNetwork{},
 }
 
 // Resource represents a resource and an importer that can import it.
