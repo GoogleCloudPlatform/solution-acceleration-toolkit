79adeb47c5609d010666a9306fca0935ee83cbf3
Martin Petkov <mpetkov@google.com>
2020-05-15 17:03:29 -0400

    Importer: Add google_compute_instance

--- internal/tfimport/tfimport.go
+++ internal/tfimport/tfimport.go
@@ -51,6 +51,7 @@ var importers = map[string]resourceImporter{
 	"google_compute_subnetwork":            &importer.ComputeSubnetwork{},
 	"google_compute_router":                &importer.ComputeRouter{},
 	"google_compute_router_nat":            &importer.ComputeRouterNat{},
+	"google_compute_instance":              &importer.ComputeInstance{},
 }
 
 // Resource represents a resource and an importer that can import it.
