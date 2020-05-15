958c0bb520f270bbb453caf0a2a5fd9524b4e028
Martin Petkov <mpetkov@google.com>
2020-05-15 14:44:06 -0400

    Importer: Add google_dns_record_set

--- internal/tfimport/tfimport.go
+++ internal/tfimport/tfimport.go
@@ -51,6 +51,7 @@ var importers = map[string]resourceImporter{
 	"google_compute_subnetwork":            &importer.ComputeSubnetwork{},
 	"google_compute_router":                &importer.ComputeRouter{},
 	"google_compute_router_nat":            &importer.ComputeRouterNat{},
+	"google_dns_record_set":                &importer.DNSRecordSet{},
 }
 
 // Resource represents a resource and an importer that can import it.
