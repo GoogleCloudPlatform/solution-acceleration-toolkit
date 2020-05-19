6fc64a26c8b8c84b839fd50db348ec0c6a252180
Martin Petkov <MartinPetkov@users.noreply.github.com>
2020-05-19 14:49:32 -0400

    Importer: Add google_dns_record_set (#119)
    * Importer: Add google_dns_record_set
    
    * Fix formatting.

--- internal/tfimport/tfimport.go
+++ internal/tfimport/tfimport.go
@@ -51,6 +51,7 @@ var importers = map[string]resourceImporter{
 	"google_compute_subnetwork":              &importer.ComputeSubnetwork{},
 	"google_compute_router":                  &importer.ComputeRouter{},
 	"google_compute_router_nat":              &importer.ComputeRouterNat{},
+	"google_dns_record_set":                  &importer.DNSRecordSet{},
 	"google_firebase_project":                &importer.FirebaseProject{},
 	"google_project_iam_custom_role":         &importer.ProjectIAMCustomRole{},
 	"google_pubsub_topic":                    &importer.PubSubTopic{},
