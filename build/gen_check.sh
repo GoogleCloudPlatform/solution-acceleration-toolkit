8cddfe0f392ef1b9405d4cb82b2fc8cdf7c712d7
Martin Petkov <MartinPetkov@users.noreply.github.com>
2020-06-08 12:28:05 -0400

    Add CI check for generated files (#255)
    * Add CI check for generated files
    
    Fixes #254
    
    * Fix script being called
    
    * Add author to git init commit
    
    * Add email to commit author
    
    * Omg git
    
    * Adjust wording
    
    * Use just git-diff
    
    * Use normal diff, not git
    
    Much simpler this way
    
    * Ignore top-level README.md
    
    This is not part of the generated output, it's there to explain what this directory is.
    
    * Run gen check in parallel
    
    * Remove bad trap signals

---
+++ build/gen_check.sh
@@ -0,0 +1,40 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This file ensures that the checked-in generated examples are up-to-date.
+
+#!/bin/bash
+set -e
+
+# Generate in a tmp dir, then diff with examples.
+tmp="$(mktemp -d)"
+trap "rm -rf '${tmp}'" EXIT INT TERM
+examples='examples/policygen/generated'
+
+cmd='go run ./cmd/policygen --config_path=examples/policygen/config.yaml --state_path examples/policygen/example.tfstate --output_path'
+${cmd} "${tmp}"
+
+# Don't use diff -x because that matches basenames in any directory.
+# We specifically want to exclude only the top-level README.md.
+changed="$(diff -r ./${examples} ${tmp} | grep -v ': README.md')" || true
+if [[ -n "${changed}" ]]; then
+  cat <<EOF
+The following generated files have changes:
+${changed}
+
+Please run the following command and check in the changes:
+${cmd} ${examples}
+EOF
+  exit 1
+fi
