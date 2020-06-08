88e06b8abd248e5b3605d35418432130c70d575d
Martin Petkov <mpetkov@google.com>
2020-06-08 13:06:29 -0400

    Add engine-generated configs and update gen-check

---
+++ examples/tfengine/generated/full/live/prod/team1/example-prod-apps/resources/main.tf
@@ -0,0 +1,44 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+terraform {
+  required_version = "~> 0.12.0"
+  required_providers {
+    google      = "~> 3.0"
+    google-beta = "~> 3.0"
+  }
+  backend "gcs" {
+  }
+}
+
+
+module "project_iam_members" {
+  source   = "terraform-google-modules/iam/google//modules/projects_iam"
+  version  = "~> 6.1.0"
+
+  projects = [var.project_id]
+  mode     = "additive"
+
+  bindings = {
+    "roles/container.viewer" = [
+      "group:example-viewers@example.com",
+    ],
+  }
+}
+
+resource "google_service_account" "example_sa" {
+  account_id = "example-sa"
+  project    = var.project_id
+}
+
---
+++ examples/tfengine/generated/full/live/prod/team1/example-prod-networks/resources/variables.tf
@@ -0,0 +1,5 @@
+
+variable "project_id" {
+  type = string
+}
+
