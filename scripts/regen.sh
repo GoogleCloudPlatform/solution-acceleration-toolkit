2e594a6fca3bca0ae262362ca47a92dad7db891b
Martin Petkov <MartinPetkov@users.noreply.github.com>
2020-06-09 13:46:55 -0400

    Add helper script to regen generated folders (#270)
    * Add helper script to regen generated folders
    
    * Add license

---
+++ scripts/regen.sh
@@ -0,0 +1,20 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/usr/bin/env bash
+
+# Helper to regenerate generated files.
+go run ./cmd/policygen --config_path examples/policygen/config.yaml --output_path examples/policygen/generated --state_path examples/policygen/example.tfstate
+go run ./cmd/tfengine --config_path examples/tfengine/simple.hcl --output_path examples/tfengine/generated/simple
+go run ./cmd/tfengine --config_path examples/tfengine/full.hcl --output_path examples/tfengine/generated/full
