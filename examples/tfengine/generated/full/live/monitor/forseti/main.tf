db2fcce3237cd18b48de1dc85346a9b295af0374
umairidris <umairidris@google.com>
2020-06-23 21:38:55 -0400

    regen

--- examples/tfengine/generated/full/live/monitor/forseti/main.tf
+++ examples/tfengine/generated/full/live/monitor/forseti/main.tf
@@ -88,4 +88,8 @@ module "forseti" {
 
   manage_rules_enabled     = false
   config_validator_enabled = true
+
+  # Enable Cloud Security Command Center (CSCC) notification.
+  cscc_violations_enabled  = true
+  cscc_source_id           = "organizations/12345678/sources/88888888"
 }
