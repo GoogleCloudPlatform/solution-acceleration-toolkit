15622d00fbbb9e44d8fdcf8b73d1243e18f6cf12
Xin <xingao267@users.noreply.github.com>
2020-08-25 11:35:58 -0400

    support google_*_iam_bindings in policygen (#559)
    * support google_*_iam_bindings in policygen
    
    * fix integration tests
    
    * address code review comments
    
    * address comments
    
    * sort strings list in tests for flakeness

--- internal/policygen/iam.go
+++ internal/policygen/iam.go
@@ -95,7 +95,7 @@ func generateIAMPolicies(rn runner.Runner, resources []*states.Resource, outputP
 func allBindings(rn runner.Runner, resources []*states.Resource) (map[root]roleBindings, error) {
 
 	// All roles associated with a root resource (organization, folder or project).
-	var bindings = make(map[root]roleBindings)
+	var allBindings = make(map[root]roleBindings)
 
 	typeToIDField := map[string]string{
 		"project":      "project",
@@ -103,55 +103,158 @@ func allBindings(rn runner.Runner, resources []*states.Resource) (map[root]roleB
 		"organization": "org_id",
 	}
 
-	for t, idField := range typeToIDField {
-		// TODO(https://github.com/GoogleCloudPlatform/healthcare-data-protection-suite/issues/152): Support google_*_iam_bindings.
-		resourceType := fmt.Sprintf("google_%s_iam_member", t)
-		instances, err := terraform.GetInstancesForType(resources, resourceType)
+	for rootType, idField := range typeToIDField {
+		iamMembers, err := members(rn, resources, rootType, idField)
 		if err != nil {
-			return nil, fmt.Errorf("get resource instances for type %q: %v", resourceType, err)
+			return nil, err
 		}
 
-		for _, ins := range instances {
-			if err := validate(ins, []string{idField, "role", "member"}); err != nil {
-				return nil, err
-			}
+		iamBindings, err := bindings(rn, resources, rootType, idField)
+		if err != nil {
+			return nil, err
+		}
 
-			id := ins[idField].(string) // Type checked in validate()
-			// For projects, the ID in the state is the project ID, but we need project number in policies.
-			if t == "project" {
-				if id, err = projectNumber(rn, id); err != nil {
-					return nil, err
+		// Add iamBindings to iamMembers.
+		// If iamMembers have members for the same root and role, replace it with the value from iamBindings.
+		for root, bindings := range iamBindings {
+			for role, members := range bindings {
+				// Init the roleBindings map if it didn't exist.
+				if _, ok := iamMembers[root]; !ok {
+					iamMembers[root] = make(roleBindings)
 				}
-			} else if t == "folder" {
-				// For folders, the ID can be either {folder_id} or folders/{folder_id}. Remove the 'folders/' prefix if exists.
-				id = strings.TrimPrefix(id, "folders/")
+				iamMembers[root][role] = members
 			}
-
-			key := root{Type: t, ID: id}
-
-			// Init the roleBindings map if it didn't exist.
-			if _, ok := bindings[key]; !ok {
-				bindings[key] = make(roleBindings)
-			}
-
-			role := ins["role"].(string)
-			bindings[key][role] = append(bindings[key][role], ins["member"].(string))
 		}
+		for root, bindings := range iamMembers {
+			allBindings[root] = bindings
+		}
+	}
+	return allBindings, nil
+}
+
+// members returns role bindings map for google_%s_iam_member (non-authoritative).
+func members(rn runner.Runner, resources []*states.Resource, rootType, idField string) (map[root]roleBindings, error) {
+	var bindings = make(map[root]roleBindings)
+	resourceType := fmt.Sprintf("google_%s_iam_member", rootType) // non-authoritative
+	instances, err := terraform.GetInstancesForType(resources, resourceType)
+	if err != nil {
+		return nil, fmt.Errorf("get resource instances for type %q: %v", resourceType, err)
+	}
+
+	for _, ins := range instances {
+		if err := validateMandatoryStringFields(ins, []string{idField, "role", "member"}); err != nil {
+			return nil, err
+		}
+
+		id, err := normalizeID(rn, rootType, ins[idField].(string)) // Type checked in validate function.
+		if err != nil {
+			return nil, fmt.Errorf("normalize root resource ID: %v", err)
+		}
+
+		key := root{Type: rootType, ID: id}
+
+		// Init the roleBindings map if it didn't exist.
+		if _, ok := bindings[key]; !ok {
+			bindings[key] = make(roleBindings)
+		}
+
+		role := ins["role"].(string)
+		bindings[key][role] = append(bindings[key][role], ins["member"].(string))
 	}
 	return bindings, nil
 }
 
-// validate checks the presence of mandatory fields and assert string type.
-func validate(instance map[string]interface{}, mandatoryFields []string) error {
+// bindings returns role bindings map for google_%s_iam_binding (authoritative).
+func bindings(rn runner.Runner, resources []*states.Resource, rootType, idField string) (map[root]roleBindings, error) {
+	var bindings = make(map[root]roleBindings)
+	resourceType := fmt.Sprintf("google_%s_iam_binding", rootType) // authoritative for a given role
+	instances, err := terraform.GetInstancesForType(resources, resourceType)
+	if err != nil {
+		return nil, fmt.Errorf("get resource instances for type %q: %v", resourceType, err)
+	}
+
+	for _, ins := range instances {
+		if err := validateMandatoryStringFields(ins, []string{idField, "role"}); err != nil {
+			return nil, err
+		}
+		if err := validateMandatoryStringLists(ins, []string{"members"}); err != nil {
+			return nil, err
+		}
+
+		id, err := normalizeID(rn, rootType, ins[idField].(string)) // Type checked in validate function.
+		if err != nil {
+			return nil, fmt.Errorf("normalize root resource ID: %v", err)
+		}
+
+		key := root{Type: rootType, ID: id}
+
+		// Init the roleBindings map if it didn't exist.
+		if _, ok := bindings[key]; !ok {
+			bindings[key] = make(roleBindings)
+		}
+
+		role := ins["role"].(string)
+
+		var members []string
+		for _, s := range ins["members"].([]interface{}) {
+			members = append(members, s.(string)) // Type checked in validate function.
+		}
+
+		// There should not be more than one instance of google_%s_iam_binding for the same resource
+		// across all states. But we append all members just in case.
+		bindings[key][role] = append(bindings[key][role], members...)
+	}
+	return bindings, nil
+}
+
+// validateMandatoryStringFields checks the presence of mandatory fields and assert string type.
+func validateMandatoryStringFields(instance map[string]interface{}, mandatoryFields []string) error {
 	for _, k := range mandatoryFields {
-		if _, ok := instance[k]; !ok {
+		field, ok := instance[k]
+		if !ok {
 			return fmt.Errorf("mandatory field %q missing from instance: %v", k, instance)
 		}
-		if _, ok := instance[k].(string); !ok {
-			return fmt.Errorf("value for %q should be a string, got %T", k, instance[k])
+		if _, ok := field.(string); !ok {
+			return fmt.Errorf("value for %q should be a string, got %T", k, field)
 		}
 	}
 	return nil
+}
+
+// validateMandatoryLists checks the presence of mandatory fields and assert []interface{} type.
+func validateMandatoryStringLists(instance map[string]interface{}, mandatoryFields []string) error {
+	for _, k := range mandatoryFields {
+		field, ok := instance[k]
+		if !ok {
+			return fmt.Errorf("mandatory field %q missing from instance: %v", k, instance)
+		}
+		lst, ok := field.([]interface{})
+		if !ok {
+			return fmt.Errorf("value for %q should be a []interface{}, got %T", k, field)
+		}
+		for _, s := range lst {
+			if _, ok := s.(string); !ok {
+				return fmt.Errorf("%q should be a string, got %T", s, s)
+			}
+		}
+
+	}
+	return nil
+}
+
+func normalizeID(rn runner.Runner, t, id string) (string, error) {
+	var err error
+	nid := id
+	// For projects, the ID in the state is the project ID, but we need project number in policies.
+	if t == "project" {
+		if nid, err = projectNumber(rn, id); err != nil {
+			return "", err
+		}
+	} else if t == "folder" {
+		// For folders, the ID can be either {folder_id} or folders/{folder_id}. Remove the 'folders/' prefix if exists.
+		nid = strings.TrimPrefix(id, "folders/")
+	}
+	return nid, nil
 }
 
 func projectNumber(rn runner.Runner, id string) (string, error) {
