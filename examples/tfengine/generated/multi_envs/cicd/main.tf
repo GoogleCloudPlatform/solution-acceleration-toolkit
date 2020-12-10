da017d91418b8342ed6ef4897e9d42ac98b098e9
Xin Gao <xingao267@users.noreply.github.com>
2020-12-10 12:18:12 -0500

    use a shared env and branch for env-independent deployments (#684)

--- examples/tfengine/generated/multi_envs/cicd/main.tf
+++ examples/tfengine/generated/multi_envs/cicd/main.tf
@@ -53,8 +53,6 @@ locals {
     "serviceusage.googleapis.com",
     "sqladmin.googleapis.com",
     "sourcerepo.googleapis.com",
-    "appengine.googleapis.com",
-    "cloudscheduler.googleapis.com",
   ]
   cloudbuild_sa_viewer_roles = [
     "roles/browser",
@@ -165,34 +163,7 @@ resource "google_project_iam_member" "cloudbuild_sa_project_iam" {
   ]
 }
 
-# Cloud Scheduler resources.
-# Cloud Scheduler requires an App Engine app created in the project.
-resource "google_app_engine_application" "cloudbuild_scheduler_app" {
-  project     = var.project_id
-  location_id = "us-east1"
-  depends_on = [
-    google_project_service.services,
-  ]
-}
 
-# Service Account and its IAM permissions used for Cloud Scheduler to schedule Cloud Build triggers.
-resource "google_service_account" "cloudbuild_scheduler_sa" {
-  project      = var.project_id
-  account_id   = "cloudbuild-scheduler-sa"
-  display_name = "Cloud Build scheduler service account"
-  depends_on = [
-    google_project_service.services,
-  ]
-}
-
-resource "google_project_iam_member" "cloudbuild_scheduler_sa_project_iam" {
-  project = var.project_id
-  role    = "roles/cloudbuild.builds.editor"
-  member  = "serviceAccount:${google_service_account.cloudbuild_scheduler_sa.email}"
-  depends_on = [
-    google_project_service.services,
-  ]
-}
 
 # Cloud Build - Cloud Build Service Account IAM permissions
 # IAM permissions to allow Cloud Build Service Account use the billing account.
