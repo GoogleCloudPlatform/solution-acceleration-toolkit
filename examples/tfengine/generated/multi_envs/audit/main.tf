befea6bb6bf44b7c8a4fb6d995d30ee8bf98c8b8
Xin Gao <xingao@google.com>
2021-01-14 17:20:52 -0500

    ref module at a commit hash before released version is available

--- examples/tfengine/generated/multi_envs/audit/main.tf
+++ examples/tfengine/generated/multi_envs/audit/main.tf
@@ -28,8 +28,10 @@ terraform {
 # Deletion lien: https://cloud.google.com/resource-manager/docs/project-liens
 # Shared VPC: https://cloud.google.com/docs/enterprise/best-practices-for-enterprise-organizations#centralize_network_control
 module "project" {
-  source  = "terraform-google-modules/project-factory/google"
-  version = "~> 10.0.2"
+  # TODO(xingao): pin to released version once available.
+  source = "github.com/terraform-google-modules/project-factory/google?ref=c41ba360a6bc6800a30d284b8fa23eb3ef5a8d7f"
+  # source  = "terraform-google-modules/project-factory/google"
+  # version = "~> 10.1.0"
 
   name            = "example-audit"
   org_id          = ""
--- templates/tfengine/components/app/devops/main.tf
+++ templates/tfengine/components/app/devops/main.tf
@@ -44,8 +44,10 @@ locals {
 
 # Create the project, enable APIs, and create the deletion lien, if specified.
 module "project" {
-  source  = "terraform-google-modules/project-factory/google"
-  version = "~> 10.0.2"
+  # TODO(xingao): pin to released version once available.
+  source  = "github.com/terraform-google-modules/project-factory/google?ref=c41ba360a6bc6800a30d284b8fa23eb3ef5a8d7f"
+  # source  = "terraform-google-modules/project-factory/google"
+  # version = "~> 10.1.0"
   name                    = "${local.constants.project_prefix}-${local.constants.env_code}-devops"
   org_id                  = ""
   folder_id               = local.constants.folder_id
