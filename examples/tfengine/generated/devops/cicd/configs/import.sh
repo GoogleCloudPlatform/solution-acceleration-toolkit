8e6193db10a0ef97edb29e2e0607cfd00f6fbb9a
Xin Gao <xingao267@users.noreply.github.com>
2021-02-12 12:15:54 -0500

    minor change in usage log of import.sh (#798)

--- examples/tfengine/generated/devops/cicd/configs/import.sh
+++ examples/tfengine/generated/devops/cicd/configs/import.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -ex
+set -e
 
 DIRS=()
 
@@ -23,7 +23,7 @@ do
   case $c in
     d) DIRS="${OPTARG}" ;;
     *)
-      echo "Invalid flag ${OPTARG}"
+      echo "Usage: import.sh -d=\"dir1 dir2 ...\""
       exit 1
       ;;
   esac
