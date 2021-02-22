0a460e9aa9804f1509f7662a4a28882bd31fd897
Xin Gao <xingao@google.com>
2021-02-22 14:35:16 -0500

    also import service account in import.sh

--- examples/tfengine/generated/folder_foundation/cicd/configs/import.sh
+++ examples/tfengine/generated/folder_foundation/cicd/configs/import.sh
@@ -39,7 +39,11 @@ chmod +x ${ROOT}/tfimport
 
 for mod in "${DIRS[@]}"
 do
-  ${ROOT}/tfimport --input_dir="${ROOT}"/"${mod}" --resource_types='google_project' --resource_types='google_project_service' --interactive=false || true
+  ${ROOT}/tfimport --input_dir="${ROOT}"/"${mod}" \
+    --resource_types='google_project' \
+    --resource_types='google_project_service' \
+    --resource_types='google_service_account' \
+    --interactive=false || true
 done
 
 rm -f ${ROOT}/tfimport
