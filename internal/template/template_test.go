d5750c17c71ccb49125cb864c51bfccade45c87b
yeweidaniel <danielye@google.com>
2021-03-15 14:34:05 -0400

    Add make slice custom function. (#817)
    * Add make slice custom function.
    
    * Add test
    
    * fix go formatting
    
    Co-authored-by: Martin Petkov <MartinPetkov@users.noreply.github.com>
    Co-authored-by: Xin Gao <xingao267@users.noreply.github.com>

--- internal/template/template_test.go
+++ internal/template/template_test.go
@@ -224,3 +224,24 @@ func TestWriteBufferErr(t *testing.T) {
 func intPointer(i int) *int {
 	return &i
 }
+
+func TestMakeSlice(t *testing.T) {
+	tests := []struct {
+		inputs, want []interface{}
+	}{
+		{
+			inputs: []interface{}{"a", "b", "c"},
+			want:   []interface{}{"a", "b", "c"},
+		},
+		{
+			inputs: []interface{}{1, 2, 3},
+			want:   []interface{}{1, 2, 3},
+		},
+	}
+	for _, tc := range tests {
+		got := makeSlice(tc.inputs...)
+		if diff := cmp.Diff(tc.want, got); diff != "" {
+			t.Errorf("makeSlice result differs (-want +got):\n%v", diff)
+		}
+	}
+}
