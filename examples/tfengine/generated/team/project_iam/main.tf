afd0e73e98c8ad957dc346b422d2739313da615c
Pasha Ghabussi <pashag@google.com>
2021-07-13 22:07:22 +0000

    Add auto-generated files after running ./regen.sh

---
+++ examples/tfengine/generated/team/project_iam/main.tf
@@ -0,0 +1,77 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+terraform {
+  required_version = ">=0.14"
+  required_providers {
+    google      = "~> 3.0"
+    google-beta = "~> 3.0"
+    kubernetes  = "~> 1.0"
+  }
+  backend "gcs" {
+    bucket = "example-terraform-state"
+    prefix = "project_iam"
+  }
+}
+
+# Create the project and optionally enable APIs, create the deletion lien and add to shared VPC.
+# Deletion lien: https://cloud.google.com/resource-manager/docs/project-liens
+# Shared VPC: https://cloud.google.com/docs/enterprise/best-practices-for-enterprise-organizations#centralize_network_control
+module "project" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 11.1.0"
+
+  name            = "example-prod-iam"
+  org_id          = ""
+  folder_id       = "12345678"
+  billing_account = "000-000-000"
+  lien            = true
+  # Create and keep default service accounts when certain APIs are enabled.
+  default_service_account = "keep"
+  # Do not create an additional project service account to be used for Compute Engine.
+  create_project_sa = false
+  # When Kubernetes Engine API is enabled, grant Kubernetes Engine Service Agent the
+  # Compute Security Admin role on the VPC host project so it can manage firewall rules.
+  # It is a no-op when Kubernetes Engine API is not enabled in the project.
+  grant_services_security_admin_role = true
+  activate_apis                      = []
+}
+
+module "storage_bucket_iam_bindings_0" {
+  source = "terraform-google-modules/iam/google//modules/storage_buckets_iam"
+  storage_buckets = [
+    "example-bucket",
+  ]
+  mode = "additive"
+
+  bindings = {
+    "roles/storage.admin" = [
+      "serviceAccount:runner@example-prod-apps.iam.gserviceaccount.com",
+    ],
+  }
+}
+
+module "project_iam_bindings_1" {
+  source = "terraform-google-modules/iam/google//modules/projects_iam"
+  projects = [
+    "example-prod-data",
+  ]
+  mode = "additive"
+
+  bindings = {
+    "roles/browser" = [
+      "serviceAccount:runner@example-prod-apps.iam.gserviceaccount.com",
+    ],
+  }
+}
