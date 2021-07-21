59aad1c027fc8ecf9f948b2c0e53098e69e36f94
ernestognw <ernestognw@gmail.com>
2021-07-21 15:34:51 -0500

    Fix typo

--- templates/tfengine/components/cicd/triggers/plan/main.tf
+++ templates/tfengine/components/cicd/triggers/plan/main.tf
@@ -112,7 +112,7 @@ resource "google_cloud_scheduler_job" "plan_scheduler_env" {
       scope = "https://www.googleapis.com/auth/cloud-platform"
       service_account_email = "${google_service_account.cloudbuild_scheduler_sa.email}"
     }
-    uri = "https://cloudbuild.googleapis.com/v1/${google_cloudbuild_trigger.plan_scheduled_env}.id}:run"
+    uri = "https://cloudbuild.googleapis.com/v1/${google_cloudbuild_trigger.plan_scheduled_env.id}:run"
     body = base64encode("{\"branchName\":\"${var.branch_name}\"}")
   }
 }
