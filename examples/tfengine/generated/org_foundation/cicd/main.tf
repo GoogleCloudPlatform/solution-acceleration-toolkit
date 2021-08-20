2283ac5b29be18fd841f63829fd9862968fdcb89
ernestognw <ernestognw@gmail.com>
2021-08-20 11:42:50 -0500

    Rename environment triggers for triggers

--- examples/tfengine/generated/org_foundation/cicd/main.tf
+++ examples/tfengine/generated/org_foundation/cicd/main.tf
@@ -260,7 +260,7 @@ resource "google_folder_iam_member" "cloudbuild_sa_folder_iam" {
 }
 
 # Create Google Cloud Build triggers for specified environments
-module "environment_triggers" {
+module "triggers" {
   for_each = { for env in var.envs : env.name => env }
   source   = "./env"
 
