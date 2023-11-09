9a9ac480dee5db6af55a449eadc0778d4d516cbb
JosieLi-Google <yifangli@google.com>
2023-11-09 18:56:59 +0000

    correct project_data/main.tf

--- examples/tfengine/generated/team/project_data/main.tf
+++ examples/tfengine/generated/team/project_data/main.tf
@@ -170,23 +170,12 @@ module "healthcare_dataset" {
           bigquery_destination = {
             dataset_uri = "bq://example-prod-data.${module.one_billion_ms_dataset.bigquery_dataset.dataset_id}"
             schema_config = {
-<<<<<<< HEAD
               last_updated_partition_config {
-  expiration_ms = 1e+06
-  type          = "HOUR"
-}
-
-recursive_structure_depth = 3
-
-schema_type = "ANALYTICS"
-=======
-              recursive_structure_depth = 3
-              last_updated_partition_config = {
+                expiration_ms = 1e+06
                 type          = "HOUR"
-                expiration_ms = 1000000
               }
+              recursive_structure_depth = 3
               schema_type = "ANALYTICS"
->>>>>>> parent of b6606db (add support for lastUpdatedPartitionConfig)
             }
           }
           resource_types = ["Patient"]
