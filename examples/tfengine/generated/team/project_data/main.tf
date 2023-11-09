ea6906a63e88d1745a8f7e4d873cdd3b59d0cbfb
JosieLi-Google <yifangli@google.com>
2023-11-09 21:50:01 +0000

    ./scripts/regen.sh

--- examples/tfengine/generated/team/project_data/main.tf
+++ examples/tfengine/generated/team/project_data/main.tf
@@ -171,11 +171,7 @@ module "healthcare_dataset" {
             dataset_uri = "bq://example-prod-data.${module.one_billion_ms_dataset.bigquery_dataset.dataset_id}"
             schema_config = {
               last_updated_partition_config = {
-                expiration_ms = 1e+06
-                type          = "HOUR"
               }
-              recursive_structure_depth = 3
-              schema_type = "ANALYTICS"
             }
           }
           resource_types = ["Patient"]
