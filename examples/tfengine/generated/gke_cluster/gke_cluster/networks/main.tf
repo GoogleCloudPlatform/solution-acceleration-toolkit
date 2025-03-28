dbeb7bf268546662c67bf42d1b77570887a6fa5f
Katherine Baillie <kbaillie@google.com>
2025-03-28 17:55:27 +0000

    upgrade networks module to 7.5.0. Upgrade the project factory version to 14.4.0, to be compatible with google provider version 5.18.0

--- examples/tfengine/generated/gke_cluster/gke_cluster/networks/main.tf
+++ examples/tfengine/generated/gke_cluster/gke_cluster/networks/main.tf
@@ -50,7 +50,7 @@ module "project" {
 
 module "network" {
   source  = "terraform-google-modules/network/google"
-  version = "~> 4.0.1"
+  version = "~> 7.5.0"
 
   network_name = "network"
   project_id   = module.project.project_id
