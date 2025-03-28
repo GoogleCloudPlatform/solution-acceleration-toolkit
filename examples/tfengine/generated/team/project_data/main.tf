416900356bf55b6492eca67f0fb24efb42feafb0
Katherine Baillie <kbaillie@google.com>
2025-03-28 17:55:27 +0000

    Upgrade the project factory version to 14.4.0, to be compatible with google provider version 5.18.0

--- examples/tfengine/generated/team/project_data/main.tf
+++ examples/tfengine/generated/team/project_data/main.tf
@@ -38,7 +38,7 @@ data "google_secret_manager_secret_version" "db_password" {
 # Shared VPC: https://cloud.google.com/docs/enterprise/best-practices-for-enterprise-organizations#centralize_network_control
 module "project" {
   source  = "terraform-google-modules/project-factory/google"
-  version = "~> 11.3.0"
+  version = "~> 14.4.0"
 
   name            = "example-prod-data"
   org_id          = ""
